b1df921e729055565b3132fa5a5bc242116c6de4
Robert Safin <robertsafin98@gmail.com>
2023-04-28 09:12:37 +0800

    re

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -24,27 +24,29 @@ const Header: FC = () => {
 
 
         <motion.div
-          whileHover={{ scale: 1.4 }}
+          whileHover={{ scale: 1.1 }}
           whileTap={{ scale: 0.9 }}>
           <FaThList className={styles.logo} />
         </motion.div>
 
 
         <motion.div
-          whileHover={{ scale: 1.4, color:'#FC467D' }}
+          whileHover={{ scale: 1.1, color:'#FC467D' }}
           whileTap={{ scale: 0.9 }}>
           <h1 className={styles.title}>FavoList</h1>
         </motion.div>
 
         <motion.div
-          whileHover={{ scale: 1.4, color:'#FC467D' }}
+          whileHover={{ scale: 1.1, color:'#FC467D' }}
           whileTap={{ scale: 0.9 }}>
           <GiHamburgerMenu className={styles.burger} onClick={() => setIsShowMennu(!isShowMenu)} />
         </motion.div>
 
       </div>
+
       {isShowMenu &&
-        <motion.div initial={{ opacity: 0, scale: 0.1 }}
+        <motion.div
+          initial={{ opacity: 0, scale: 0.1 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.2 }}
           className={styles.links}>
@@ -52,8 +54,8 @@ const Header: FC = () => {
           <div className={styles.linkItem}
             onMouseEnter={() => setShowSearch(true)}
             onMouseLeave={() => setShowSearch(false)}
-
           >
+
             <RxMagnifyingGlass
               className={styles.icon} />{showSearch && <motion.div
                 initial={{ opacity: 0, scale: 0.1 }}
@@ -64,8 +66,8 @@ const Header: FC = () => {
           <div className={styles.linkItem}
             onMouseEnter={() => setShowAccount(true)}
             onMouseLeave={() => setShowAccount(false)}
-
           >
+
             <FaUser
               className={styles.icon} />{showAccount && <motion.div
                 initial={{ opacity: 0, scale: 0.1 }}
@@ -76,8 +78,8 @@ const Header: FC = () => {
           <div className={styles.linkItem}
             onMouseEnter={() => setShowSettings(true)}
             onMouseLeave={() => setShowSettings(false)}
-
           >
+
             <AiFillSetting
               className={styles.icon} />{showSettings && <motion.div
                 initial={{ opacity: 0, scale: 0.1 }}
@@ -88,8 +90,8 @@ const Header: FC = () => {
           <div className={styles.linkItem}
             onMouseEnter={() => setShowAdd(true)}
             onMouseLeave={() => setShowAdd(false)}
-
           >
+
             <HiOutlinePlus
               className={styles.icon} />{showAdd && <motion.div
                 initial={{ opacity: 0, scale: 0.1 }}
