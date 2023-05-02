ad6ca8baf9323c178e71358c39ae8089461f596c
ante andrijasevic <ante.adc@gmail.com>
2023-05-02 11:28:04 +0800

    image card and title

--- next.config.js
+++ next.config.js
@@ -8,6 +8,12 @@ const nextConfig = {
         port: '',
         pathname: '/u/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'ouraring-images.imgix.net',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   reactStrictMode: true,
