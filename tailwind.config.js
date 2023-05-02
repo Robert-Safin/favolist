f65471a2c09d559e84427addf18e2fea6126a90c
ante andrijasevic <ante.adc@gmail.com>
2023-05-02 18:06:38 +0800

    cards advancement

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,5 +24,11 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  corePlugins: {
+    aspectRatio: false,
+  },
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+    // ...
+  ]
 }
