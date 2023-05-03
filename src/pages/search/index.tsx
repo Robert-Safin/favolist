85f80146ac6d2d4eef6705654dae68eec3ab4d3e
Robert Safin <robertsafin98@gmail.com>
2023-05-03 17:25:26 +0800

    search render

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,28 +1,34 @@
-import SearchBar from '@/components/searchBar/SearchBar'
-import {NextPage} from 'next'
-import { FormEventHandler } from 'react'
+import SearchBar from '@/components/searchBar/SearchBar';
+import { ListModelSchema } from '@/db/models/List';
+import { ProductModelSchema } from '@/db/models/Product';
+import { UserModelSchema } from '@/db/models/User';
+import { NextPage } from 'next';
+import { FormEventHandler, useState } from 'react';
 
-interface Props {
-
-}
-
-
-
+interface Props {}
 
 const SearchPage: NextPage<Props> = (props) => {
-  const handleSearch = async(query: string) => {
+  // Add state for foundUsers, foundLists, and foundProducts
+  const [foundUsers, setFoundUsers] = useState([]);
+  const [foundLists, setFoundLists] = useState([]);
+  const [foundProducts, setFoundProducts] = useState([]);
+
+  const handleSearch = async (query: string) => {
     try {
       const response = await fetch('/api/search', {
-        method: "POST",
+        method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify(query)
-      })
-      const data = await response.json()
-      console.log(data);
+        body: JSON.stringify(query),
+      });
+      const data = await response.json();
 
-    } catch(error) {
+      // Update the state with the fetched data
+      setFoundUsers(JSON.parse(data.foundUsers));
+      setFoundLists(JSON.parse(data.foundLists));
+      setFoundProducts(JSON.parse(data.foundProducts));
+    } catch (error) {
       console.log(error);
     }
   };
@@ -34,9 +40,28 @@ const SearchPage: NextPage<Props> = (props) => {
   return (
     <>
       <SearchBar handleSubmit={handleSubmit} handleSearch={handleSearch} />
+      <div>
+        <h2>Users:</h2>
+        {foundUsers.map((user:UserModelSchema) => (
+          <div key={String(user._id)}>{user.username}</div>
+        ))}
+      </div>
+
+      <div>
+        <h2>Lists:</h2>
+        {foundLists.map((list:ListModelSchema) => (
+          <div key={String(list._id)}>{list.title}</div>
+        ))}
+      </div>
+
+      <div>
+        <h2>Products:</h2>
+        {foundProducts.map((product:ProductModelSchema) => (
+          <div key={String(product._id)}>{product.productName}</div>
+        ))}
+      </div>
     </>
   );
 };
 
-
-export default SearchPage
+export default SearchPage;
