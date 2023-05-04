d73209d4af0f74e455d8702cb06d92979a8a4fb1
Robert Safin <robertsafin98@gmail.com>
2023-05-04 15:56:02 +0800

    ante make component

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,14 +1,16 @@
+import FoundListCard from '@/components/search-results/FoundListCard';
+import FoundProductCard from '@/components/search-results/FoundProductCard';
+import FoundUserCard from '@/components/search-results/FoundUserCard';
 import SearchBar from '@/components/searchBar/SearchBar';
 import { ListModelSchema } from '@/db/models/List';
 import { ProductModelSchema } from '@/db/models/Product';
 import { UserModelSchema } from '@/db/models/User';
 import { NextPage } from 'next';
 import { FormEventHandler, useState } from 'react';
-
-interface Props {}
+import styles from './index.module.css'
+interface Props { }
 
 const SearchPage: NextPage<Props> = (props) => {
-  // Add state for foundUsers, foundLists, and foundProducts
   const [foundUsers, setFoundUsers] = useState<UserModelSchema[]>([]);
   const [foundLists, setFoundLists] = useState<ListModelSchema[]>([]);
   const [foundProducts, setFoundProducts] = useState<ProductModelSchema[]>([]);
@@ -28,10 +30,12 @@ const SearchPage: NextPage<Props> = (props) => {
       setFoundUsers(JSON.parse(data.foundUsers));
       setFoundLists(JSON.parse(data.foundLists));
       setFoundProducts(JSON.parse(data.foundProducts));
+
     } catch (error) {
       console.log(error);
     }
   };
+
 
   const handleSubmit: FormEventHandler = (event) => {
     event.preventDefault();
@@ -40,24 +44,54 @@ const SearchPage: NextPage<Props> = (props) => {
   return (
     <>
       <SearchBar handleSubmit={handleSubmit} handleSearch={handleSearch} />
-      <div>
+      <div className={styles.resultsContainer}>
         <h2>Users:</h2>
-        {foundUsers.map((user:UserModelSchema) => (
-          <div key={String(user._id)}>{user.username}</div>
+        {foundUsers.map((user: UserModelSchema) => (
+          <FoundUserCard
+            userId={user._id}
+            key={String(user._id)}
+            username={user.username}
+            avatar={user.avatar!}
+            bio={user.bio}
+            follows={user.follows}
+            followers={user.followers}
+            lists={user.lists}
+            products={user.products}
+          />
         ))}
+
       </div>
 
-      <div>
+      <div className={styles.resultsContainer}>
         <h2>Lists:</h2>
-        {foundLists.map((list:ListModelSchema) => (
-          <div key={String(list._id)}>{list.title}</div>
+        {foundLists.map((list: ListModelSchema) => (
+          <FoundListCard
+            key={String(list._id)}
+            userId={list.user_id}
+            listId={list._id}
+            title={list.title}
+            thumbnail={list.thumbnail}
+            about={list.about}
+            products={list.products}
+          />
         ))}
       </div>
 
-      <div>
+      <div className={styles.resultsContainer}>
         <h2>Products:</h2>
-        {foundProducts.map((product:ProductModelSchema) => (
-          <div key={String(product._id)}>{product.productName}</div>
+        {foundProducts.map((product: ProductModelSchema) => (
+          <FoundProductCard key={String(product._id)}
+          productId={product.id}
+          userId={product.user_id}
+          listId={product.listId}
+          productListName={product.productListName}
+          productLogo={product.productLogo}
+          productImage={product.productImage}
+          content={product.content}
+          price={product.price}
+          referral={product.referral}
+
+          />
         ))}
       </div>
     </>
