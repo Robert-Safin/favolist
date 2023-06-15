4c7bdd4ec02f5ad0e42ec4a7024f1ae00c4c8801
Robert Safin <robertsafin98@gmail.com>
2023-06-15 21:01:41 +0800

    re

--- src/pages/api/suggestions/list.ts
+++ src/pages/api/suggestions/list.ts
@@ -1,37 +1,43 @@
 import { List, User } from "@/db/models";
-
 import { NextApiRequest, NextApiResponse } from "next";
 import { getToken } from "next-auth/jwt";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     const token = await getToken({ req });
+    if (!token) {
+      res.status(401).json({ error: 'Not authorized' });
+      return;
+    }
 
-    const userDoc = await User.findOne({ email: token?.email });
+    const userDoc = await User.findOne({ email: token?.email }).populate('follows');
+    if (!userDoc) {
+      res.status(404).json({ error: 'User not found' });
+      return;
+    }
 
     const getRandomValue = (arr: any[]) => {
       let randomIndex = Math.floor(Math.random() * arr.length);
       return arr[randomIndex];
     };
 
-    if (userDoc?.follows.length! > 0) {
-      const randomFriendId = getRandomValue(userDoc?.follows!);
-      const randomFriendDoc = await User.findOne({ _id: randomFriendId });
-      const randomFriendListId = getRandomValue(randomFriendDoc?.lists!);
-      const randomListDoc = await List.findOne({ _id: randomFriendListId });
-      const randomListOwner = await User.findOne({
-        _id: randomListDoc?.user_id,
-      });
-      console.log("listDoc:", randomListDoc, "userDoc:", randomListOwner);
+    if (userDoc?.follows?.length > 0) {
+      const randomFriendDoc = getRandomValue(userDoc?.follows);
+      if (!randomFriendDoc?.lists?.length) {
+        res.status(400).json({ message: "Friend has no lists" });
+        return;
+      }
+      const randomListId = getRandomValue(randomFriendDoc?.lists);
+      const randomListDoc = await List.findOne({ _id: randomListId });
 
-      res
-        .status(200)
-        .json({ listDoc: randomListDoc, userDoc: randomListOwner });
+      res.status(200).json({ listDoc: randomListDoc, userDoc: randomFriendDoc });
     } else {
-      res.status(400).json({ message: "not matched" });
+      res.status(400).json({ message: "User is not following anyone" });
     }
   } catch (error) {
-    console.log(error);
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
   }
 };
+
 export default handler;
