e52dc4025211cac563c30727ea5d84dc3fb6b167
Robert Safin <robertsafin98@gmail.com>
2023-06-15 20:07:20 +0800

    re

--- src/pages/api/suggestions/users.ts
+++ src/pages/api/suggestions/users.ts
@@ -5,34 +5,34 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { getToken } from "next-auth/jwt";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const token = await getToken({ req });
+  try {
+    const token = await getToken({ req });
 
-  if (!token) {
-    console.log('no token');
-
-  }
-
-  const userDoc = await User.findOne({ email: token?.email });
-
-  const getRandomValue = (arr: UserModelSchema[]) => {
-    let randomIndex = Math.floor(Math.random() * arr.length);
-    return arr[randomIndex];
-  };
-  if (userDoc?.follows.length !== undefined) {
-    const randomFriendId = getRandomValue(userDoc?.follows!);
-    const randomFriendDoc = await User.findOne({ _id: randomFriendId });
-    if (randomFriendDoc?.follows !== undefined) {
-      const randomFriendFriendsDoc = await randomFriendDoc?.populate("follows");
-      const filteredFollows = randomFriendFriendsDoc?.follows.filter(
-        (id) => !userDoc?.follows.includes(id)
-      );
-      res.json({ filteredFollows });
-    } else {
-      console.log('NO FRIEND FOLLOWS');
+    if (!token) {
+      console.log('no token');
 
     }
-  } else {
-    console.log('NO FOLLOWS');
+
+    const userDoc = await User.findOne({ email: token?.email });
+
+    const getRandomValue = (arr: UserModelSchema[]) => {
+      let randomIndex = Math.floor(Math.random() * arr.length);
+      return arr[randomIndex];
+    };
+    if (userDoc?.follows.length !== undefined) {
+      const randomFriendId = getRandomValue(userDoc?.follows!);
+      const randomFriendDoc = await User.findOne({ _id: randomFriendId });
+      if (randomFriendDoc?.follows !== undefined) {
+        const randomFriendFriendsDoc = await randomFriendDoc?.populate("follows");
+        const filteredFollows = randomFriendFriendsDoc?.follows.filter(
+          (id) => !userDoc?.follows.map(follow => follow.toString()).includes(id.toString())
+      );
+        res.json({ filteredFollows });
+      }
+    }
+
+  } catch(error) {
+    console.log(error);
 
   }
 };
